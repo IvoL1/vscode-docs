0d67b2ecda22791a28b8068bcca4d2d257e6336d
Ivo Luis <ivoluis239@gmail.com>
2025-07-18 04:25:49 -0300

    Create health.router.ts

---
+++ health.router.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+const router = Router();
+
+router.get('/health', (req, res) => {
+  res.json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+export default router;
